fc8814e6f9993a8f7cd3a1b492293070146fc17e
shompot <cholpon94@gmail.com>
2017-08-24 10:44:57 +0600

    The whole Template is ready
    Header and Footer are aligned.
    Body is reade to have posts.
    scrollbar is done.
    Only need to add some posts.

--- js/blog.js
+++ js/blog.js
@@ -1,1 +1,16 @@
+$(document).ready (function (){
 
+   var screenH = $(window).height();
+   var headerH = $("#header").height();
+   var footerH = $("#footer").height();
+   var bodyH = screenH - headerH - footerH - $('#header').offset().top;
+
+   $('#body').css ({
+      height: bodyH
+   });
+
+   $('#footer').css({
+      left: $('#header').offset().left,
+      width: $('#header').width()
+   });
+});

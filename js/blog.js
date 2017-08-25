317086c2c6c088be8f4f36443d3fc9b64499ca46
shompot <cholpon94@gmail.com>
2017-08-25 15:44:15 +0600

    ololo

--- js/blog.js
+++ js/blog.js
@@ -23,7 +23,7 @@ $(document).ready (function (){
       marginRight: "25px"
    });;
 
-   var text ="In JS Picture removal detract earnest is by. Esteems met joy attempt way clothes yet demesne tedious. Replying an marianne do it an entrance advanced. Two dare say play when hold. Required bringing me material stanhill jointure is as he. Mutual indeed yet her living result matter him bed whence. Cause dried no solid no an small so still widen. Ten weather evident smiling bed against she examine its. Rendered far ";
+   var text ="In JS now Picture removal detract earnest is by. Esteems met joy attempt way clothes yet demesne tedious. Replying an marianne do it an entrance advanced. Two dare say play when hold. Required bringing me material stanhill jointure is as he. Mutual indeed yet her living result matter him bed whence. Cause dried no solid no an small so still widen. Ten weather evident smiling bed against she examine its. Rendered far ";
    console.log (text.length);
    text = text.substring (0, 400) + " ...";
 

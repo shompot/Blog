be832bb78dbe9e0f95485e388b451b05fc3efa6d
shompot <cholpon94@gmail.com>
2017-08-29 17:16:49 +0600

    posts are now from http

--- js/blog.js
+++ js/blog.js
@@ -3,54 +3,51 @@ $(document).ready (function (){
    $('#body').css ({
       height: $(window).height() - $("#header").height() - $("#footer").height() - 2*$('#header').offset().top
    });
-   // ----------FOOTER POSITION---------
-   $('#footer').css({
-      left: $('#header').offset().left,
-      width: $('#header').width(),
-      bottom: $('#header').offset().top
-   });
    // -----------POSTS CREATION-----------
    var $post = "<div class='post'><div class='blogPic'></div><div class='blogTitleCont'><h1 class='blogTitle'>Title of Post</h1></div><div class='blogTextCont'><p class='blogText'></p></div></div>";
    $("#body").append($post);
    $("#body").append($post);
    $("#body").append($post);
    // -------BLOG TEXT SET-------------
-   var i = 1;
-   var text;
-   text ="Blog Text Here ...";
+   var i;
+   var text = "Blog Text Here ...";
+   var texts = [];
+   var textPromise;
+
+   // Set default text and request the actual text
+   i = 1;
 
    $(".blogText").each (function (){
       $(this).html(i + " " + text);
-      i++;
+
+      var link = "https://raw.githubusercontent.com/shompot/Blog/master/data/blog";
+      link += i + ".txt";
+
+      textPromise = new Promise ( function (resolve, reject){
+         $.get(link, function (data){
+            text = data.substring (0, 400)  + " ...";
+            texts.push (text);
+            resolve (text);
+         },"text");
+      });
+
+      i ++;
    });
 
-   var textPromise = new Promise ( function (resolve, reject){
-      $.get("data/blog1.txt", function (data){
-         text = data.substring (0, 400)  + " ...";
-         console.log ("Inside get: " + text);
-         resolve (text);
-      },"text");
+   // When resolved set the actual text to blog text preview
+   i = 0;
+
+   textPromise.then (function (response){
+         $(".blogText").each (function (){
+            $(this).html(texts[i]);
+            console.log ("Succeeded to assign the actual blog text number " + (i+1));
+            i++;
+         });
+      }, function(){
+         $(".blogText").each (function (){
+            console.log ("Failed to assign the actual blog text");
+         });
    });
 
-   textPromise.then (function (response)
-      {
-         $(".blogText").each (function (){
-            $(this).html(response);
-            console.log ($(this).html());
-         });
-      }, function()
-      {
-         $(".blogText").each (function (){
-            $(this).html(response);
-         });
-      }
-   );
-   console.log ("New Text: " + text);
-});
-// -------BLOG TEXT CONTAINER---------
-$('.blogTextCont').css({
-   height: $('.post').height() - $('.blogTitleCont').height() - parseInt($('.blogTitleCont').css ('marginTop')) - 40,
-   left: 50,
-   marginLeft: $('.blogPic').width() + parseInt($('.blogPic').css('marginLeft')) + parseInt($('.blogTitleCont').css('marginLeft')),
-   marginRight: "25px"
+   //-------------------------------------------
 });
